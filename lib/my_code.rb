dfcce4e1f7c50ab68ea5eb92759c95da76c87bb6
Ruben Vallejo <r.will16@hotmail.com>
2019-09-24 23:16:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,22 @@ array.length.times do |index|
 end
 new_array
 end
+
+def map_to_no_changw(array)
+  new_array = []
+  array.length.times do |index|
+  new_array << array[index]
+end
+new_array
+end
+
+def map_tp_double (array)
+  new_array = []
+  array.length.times do |index|
+  new_array << array[index] * 2
+end
+new_array
+end
+
+
+  
